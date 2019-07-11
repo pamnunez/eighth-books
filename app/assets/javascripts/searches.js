73a53aa02c2171a17bcb80004beda01b269865c7
Pamela Nunez <nunezpam@gmail.com>
2019-07-10 22:26:30 -0400

    Moved onclick javascript to tr tag in search results to redirect whole row to book info link

--- app/assets/javascripts/searches.js
+++
@@ -1,9 +0,0 @@
-$(document).on('ready page:load', function(){
-    $('#results > tbody > tr').click(function(){
-        var href = $(this).find("a").attr("href");
-        console.log(href);
-        if(href) {
-            window.location = href;
-        }
-    });
-});
