9fe8199507136f94a0eef64d33261828f96fa370
Pamela Nunez <nunezpam@gmail.com>
2019-07-12 08:48:41 -0400

    Modified Redis connection to use heroku env variable for server address

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 EighthBooks::Application.config.session_store :redis_store,
-  servers: ["redis://localhost:6379/0/session"],
+  servers: [ENV['REDIS_URL']],
   expire_after: 90.minutes,
   key: "_#{Rails.application.class.parent_name.downcase}_session",
   threadsafe: false
